91933481a1ed02040619ff7eb3c468a57a9a7154
Chap Ambrose <chapambrose@gmail.com>
2018-08-01 15:23:15 -0500

    return xirr as a string

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ get '/xirr' do
   cf << Transaction.new(-1000,  date: Date::strptime('2018-01-01', "%Y-%m-%d"))
   cf << Transaction.new(-2000,  date: Date::strptime('2018-03-01', "%Y-%m-%d"))
   cf << Transaction.new( 4500, date: Date::strptime('2018-07-01', "%Y-%m-%d"))
-  return {xirr: cf.xirr}
+  return cf.xirr.to_s
 end
 
 get '/*' do
