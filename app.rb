f8e45c8df783884ed859143da894bc2f1e16ed16
Chap Ambrose <chapambrose@gmail.com>
2018-08-01 15:05:16 -0500

    add /xirr for testing

--- app.rb
+++ app.rb
@@ -1,11 +1,16 @@
 require "sinatra"
+require "xirr"
+include Xirr
 
-get '/*' do
-  puts params.to_s
-  return params.to_s
+get '/xirr' do
+  cf = Cashflow.new
+  cf << Transaction.new(-1000,  date: Date::strptime('2018-01-01', "%Y-%m-%d"))
+  cf << Transaction.new(-2000,  date: Date::strptime('2018-03-01', "%Y-%m-%d"))
+  cf << Transaction.new( 4500, date: Date::strptime('2018-07-01', "%Y-%m-%d"))
+  return {xirr: cf.xirr}
 end
 
-post '/*' do
+get '/*' do
   puts params.to_s
   return params.to_s
 end
