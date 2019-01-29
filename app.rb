472e440dfab4f5e556a7d67453598143f61b5fc2
Chap Ambrose <chapambrose@gmail.com>
2019-01-29 21:38:49 +0000

    Update app.rb

--- app.rb
+++ app.rb
@@ -1,14 +1,4 @@
 require "sinatra"
-require "xirr"
-include Xirr
-
-get '/xirr' do
-  cf = Cashflow.new
-  cf << Transaction.new(-1000,  date: Date::strptime('2018-01-01', "%Y-%m-%d"))
-  cf << Transaction.new(-2000,  date: Date::strptime('2018-03-01', "%Y-%m-%d"))
-  cf << Transaction.new( 4500, date: Date::strptime('2018-07-01', "%Y-%m-%d"))
-  return cf.xirr.to_s
-end
 
 get '/*' do
   puts 'container: ' + params.to_s
